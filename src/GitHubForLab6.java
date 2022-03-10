e1614e364da21c586d466a993e85c456a3662217
Your Name <you@example.com>
2022-03-10 14:40:50 -0500

    first commit

---
+++ src/GitHubForLab6.java
@@ -0,0 +1,12 @@
+public class GitHubForLab6 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 10;
+
+        int sum = x+y;
+
+        System.out.println(sum);
+
+    }
+
+}
